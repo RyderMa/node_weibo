55290424c5f93753b411f0e2d6b156c9905cf73d
malujie <17862835855@163.com>
2019-12-15 14:20:40 +0800

    feat: 路由演示

---
+++ src/routes/index.js
@@ -0,0 +1,33 @@
+const router = require('koa-router')()
+
+router.get('/', async (ctx, next) => {
+  await ctx.render('index', {
+    title: 'Hello Koa 2!'
+  })
+})
+
+router.get('/json', async (ctx, next) => {
+  ctx.body = {
+    title: 'koa2 json'
+  }
+})
+
+router.get('/profile/:userName', async (ctx, next) => {
+  // es6 解构
+  const { userName } = ctx.params
+  ctx.body = {
+    title: 'this is profile page for',
+    userName
+  }
+})
+
+router.get('/loadMore/:userName/:pageIndex', async (ctx, next) => {
+  const { userName, pageIndex } = ctx.params
+  ctx.body = {
+    title: 'this is profile page for',
+    userName,
+    pageIndex
+  }
+})
+
+module.exports = router
