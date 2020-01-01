85133b8e05f2caafdb6671511bde5f0f1f20a993
malujie <17862835855@163.com>
2020-01-01 20:39:18 +0800

    feat: 用户设置单元测试

--- test/login.test.js
+++ test/login.test.js
@@ -32,7 +32,7 @@ test('重复注册用户 失败', async () => {
 
 // 用户名是否存在
 test('查询已注册的用户名是否存在', async () => {
-  const res = await server.post('/api/user/isExist').send({userName})
+  const res = await server.post('/api/user/isExist').send({ userName })
   expect(res.body.errno).toBe(0)
 })
 
@@ -55,14 +55,47 @@ test('已注册，登录成功', async () => {
   console.log('cookie数据', COOKIE)
 })
 
-// // 删除
+// 修改基本信息
+test('修改基本信息成功', async () => {
+  const res = await server
+    .patch('/api/user/changeInfo')
+    .send({
+      nickName: '测试昵称',
+      city: '测试城市',
+      picture: '/test.png'
+    })
+    .set('cookie', COOKIE)
+  expect(res.body.errno).toBe(0)
+})
+
+// 修改密码
+test('修改密码成功', async () => {
+  const res = await server
+    .patch('/api/user/changePassword')
+    .send({
+      password,
+      newPassword: `p_${Date.now()}`
+    })
+    .set('cookie', COOKIE)
+  expect(res.body.errno).toBe(0)
+})
+
+// 删除
 test('删除用户，成功', async () => {
   const res = await server.post('/api/user/delete').set('cookie', COOKIE)
   expect(res.body.errno).toBe(0)
 })
 
+// 退出登录
+test('退出登录成功', async () => {
+  const res = await server
+    .post('/api/user/logout')
+    .set('cookie', COOKIE)
+  expect(res.body.errno).toBe(0)
+})
+
 // 再次查询用户
 test('删除用户后，用户名不存在', async () => {
-  const res = await server.post('/api/user/isExist').send({userName})
+  const res = await server.post('/api/user/isExist').send({ userName })
   expect(res.body.errno).not.toBe(0)
 })
